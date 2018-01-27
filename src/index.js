00ba2419e4e599014bfac082e7de0c7db2789119
taforyou <taforyou@hotmail.com>
2018-01-27 11:57:08 +0700

    Edit Index.js
    Play along with one enclosing tag

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const Test = function () {
-  return <div>Hi !!!</div>;
+  return (
+    <div>
+      <h1>Student : Good moring teacher</h1>
+      <h2>Teacher : I'm fine thank you and you ?</h2>
+      <h3>Student : I'm fine thank you</h3>
+      <h4>Student : Sit down</h4>
+    </div>
+
+  );
 }
 
 // const Test = () => {
