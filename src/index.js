38ffd0b9fc699357bb1779bd2aab04d8d7304838
taforyou <taforyou@hotmail.com>
2018-01-27 08:47:02 +0700

    Edit Index.js
    Test Arrow Function
    See how related

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Test = function () {
+  return <div>Hi !!!</div>;
+}
+
+// const Test = () => {
+//   return <div>Hi !!!</div>;
+// }
+
+ReactDOM.render(<Test />, document.getElementById('root'));
 registerServiceWorker();
