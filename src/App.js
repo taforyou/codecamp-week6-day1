9bae73b5393985dd3af763563419321c8e046392
taforyou <taforyou@hotmail.com>
2018-01-29 11:28:20 +0700

    Edit Atom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Layout, Menu, Breadcrumb,Button } from 'antd';
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+            <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+        </Header>
+
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+
+          ได้ Content ที่สวยงามมาแว้ววว แค่ Copy ล่ะวาง !!!
+          <div>
+            <Button type="primary">Button</Button>
+          </div>
+
+        </div>
+
+
+
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2016 Created by Ant UED
+      </Footer>
+      </Layout>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,21 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const Test = function () {
-  return (
-    <div>
-      <h1>Student : Good moring teacher</h1>
-      <h2>Teacher : I'm fine thank you and you ?</h2>
-      <h3>Student : I'm fine thank you</h3>
-      <h4>Student : Sit down</h4>
-    </div>
-
-  );
-}
-
-// const Test = () => {
-//   return <div>Hi !!!</div>;
-// }
-
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
