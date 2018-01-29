6d579b6efe632beacaf194f72223522dad35eabe
taforyou <taforyou@hotmail.com>
2018-01-29 16:17:31 +0700

    Edit App.js
    อธิบายเรื่อง State ใน Username // Password

--- src/App.js
+++ src/App.js
@@ -4,34 +4,51 @@ import { Form, Icon, Input, Button, Checkbox, Card } from 'antd';
 import './App.css';
 
 class App extends Component {
-  render() {
 
-    const FormItem = Form.Item;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username : '',
+      password : ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({username: event.target.value});
+  }
+
+  render() {
 
     return (
         <div className="App">
-
             <h1>Welcome to my application</h1>
             <Card title="Sing In" style={{ width: 300, margin: 'auto' }}>
               <Form className="login-form">
-                <FormItem>
-                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                </FormItem>
-                <FormItem>
+                <Form.Item>
+                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        placeholder="Username" value={this.state.username}
+                        onChange={this.handleChange}
+                     />
+                </Form.Item>
+                <Form.Item>
                     <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                </FormItem>
-                <FormItem>
+                </Form.Item>
+                <Form.Item>
                     <Button type="primary" htmlType="submit" className="login-form-button">
                       Sign in
                     </Button>
                     <a className="login-form-forgot" href="">Forgot password</a>
-                </FormItem>
+                </Form.Item>
                 <hr></hr>
-                <FormItem>
+                <Form.Item>
                     <Button type="primary" htmlType="submit" className="login-form-button">
                       Sign up
                     </Button>
-                </FormItem>
+                </Form.Item>
               </Form>
             </Card>
         </div>
